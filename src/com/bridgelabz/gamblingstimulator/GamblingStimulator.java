c1759b32a6bc4bc3f6585e53db3e18447ffaa3ff
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-01-19 13:45:57 +0530

    Added : Gambler if won or lost

--- src/com/bridgelabz/gamblingstimulator/GamblingStimulator.java
+++ src/com/bridgelabz/gamblingstimulator/GamblingStimulator.java
@@ -4,25 +4,36 @@ import java.util.Random;
 
 public class GamblingStimulator {
 	
-	static final int STAKE_MONEY = 100;
-	static final int BET_MONEY = 1;
-	static void Gamble(int num) {
-		if(num == 1 && STAKE_MONEY >= 100) {
-			int won = STAKE_MONEY + BET_MONEY;
-			System.out.println("Gambler Won : $"+BET_MONEY);
-			System.out.println("Stake Amount : "+won);
-		}
-		else {
-			int loose = STAKE_MONEY - BET_MONEY;
-			System.out.println("Gambler Loose : $"+BET_MONEY);
-			System.out.println("Stake Amount : "+loose);
-		}
-	}
+	static int STAKE_MONEY = 100;
+	static int BET_MONEY = 1;
 	
-	public static void main(String[] args) {
-
+	static void Gamble(int winpercent, int lostpercent) {
+		
 		Random random = new Random();
-		int num = random.nextInt(2);
-		Gamble(num);
+		while(STAKE_MONEY > lostpercent && STAKE_MONEY < winpercent) {
+			
+			int num = random.nextInt(2);
+			switch(num){
+				case 1 : STAKE_MONEY = STAKE_MONEY + BET_MONEY;
+					System.out.println("Gambler Won : +$"+BET_MONEY);
+					System.out.println("Stake Amount : "+STAKE_MONEY+"\n");
+					if(STAKE_MONEY == winpercent)
+						System.out.println("Gambler Resign for the Day !!!\n");
+					break;
+				
+				default : STAKE_MONEY = STAKE_MONEY - BET_MONEY;
+					System.out.println("Gambler Loose : -$"+BET_MONEY);
+					System.out.println("Stake Amount : "+STAKE_MONEY+"\n");
+					if(STAKE_MONEY == lostpercent)
+						System.out.println("Gambler Resign for the Day !!!\n");
+				}
+			}
+		}
+	public static void main(String[] args) {
+		
+		int winpercent = 150;
+		int lostpercent = 50;
+		
+		Gamble(winpercent, lostpercent);
 	}
 }
